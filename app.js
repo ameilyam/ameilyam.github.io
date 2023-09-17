7a6abcf333ab6d4c345ea47695a2068d2fe48e4a
ameilyam <ameilyamonson@gmail.com>
2023-09-16 22:12:43 -0600

    set up basic design, filled out home page and about section

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-function alertButton(){
-    alert("Hello World!");
-}
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+  
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+      });
+    });
+  });
+  
