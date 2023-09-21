1687026c1782af61c93b55ffac7d8d763d3e151c
ameilyam <ameilyamonson@gmail.com>
2023-09-21 11:42:57 -0600

    added the portfolio section and changed some styling

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-  
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-  
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
     });
   });
-  
+});
+
+
+var slideIndex = 0;
+var slideshowTimeout;
+
+showSlides();
+
+function plusSlides(n) {
+  clearTimeout(slideshowTimeout);  // Clear existing timeout
+  showSlides(slideIndex += n);
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (slideIndex >= slides.length) { slideIndex = 0 }
+  if (slideIndex < 0) { slideIndex = slides.length - 1 }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+  slideIndex++;
+  slideshowTimeout = setTimeout(showSlides, 5000); // Change slide every 5 seconds
+}
